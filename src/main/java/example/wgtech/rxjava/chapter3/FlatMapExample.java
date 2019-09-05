20682c649f60eff3597a77f3d3ca6e51fe4a9766
WangGi Lim <wgtech.dev@gmail.com>
2019-09-05 21:53:43 +0900

    Add the flatMap() example

---
+++ src/main/java/example/wgtech/rxjava/chapter3/FlatMapExample.java
@@ -0,0 +1,11 @@
+package example.wgtech.rxjava.chapter3;
+
+/**
+ *  flapMap()
+ *
+ *  - map() 을 좀 더 발전시킨 함수이다.
+ *  - map() 과는 달리, 똑같이 함수에 넣더라도 결과가 Observable로 나온다는 차이점이 있다.
+ *  즉, flatMap() 함수는 일대다 혹은 일대일 Observable 함수이다.
+ */
+public class FlatMapExample {
+}
